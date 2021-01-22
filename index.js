8f3ef23196d101bdfe192cb57aa7c1c2161cf7e4
slgu <slgu@cybozu.cn>
2021-01-22 18:28:45 +0800

    判断文件类型，增加一些输出提示

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const { config, dir } = require("./config");
 function readFileList(dir, filesList = []) {
     const files = fs.readdirSync(dir);
     files.forEach((item, index) => {
+        var extname = path.extname(item);
         var fullPath = path.join(dir, item);
         const stat = fs.statSync(fullPath);
         if (stat.isDirectory()) {
             readFileList(path.join(dir, item), filesList);
-        } else {
+        } else if (extname === '.zip') {
             filesList.push(fullPath);
+        } else {
+            return
         }
     });
     return filesList;
@@ -19,13 +22,18 @@ function readFileList(dir, filesList = []) {
 
 function uploadPlugin(zip) {
     const cmd = `kintone-plugin-uploader --domain ${config.domain} --username ${config.username} --password ${config.password} ${zip}`
-    execSync(cmd);
+    try {
+        execSync(cmd);
+    }
+    catch (error) {
+        console.log(`插件${zip}上传失败`)
+    }
 }
-
 
 const filesList = readFileList(dir);
 
-for (let file of filesList) {
-    uploadPlugin(file)
+for (let zip of filesList) {
+    console.log(`插件${zip}上传开始`)
+    uploadPlugin(zip)
 }
 console.log(`环境${config.domain}的插件上传完成`)
